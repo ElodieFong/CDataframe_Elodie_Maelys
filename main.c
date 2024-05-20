06d5e0dbe5d9b4668b9a8d6eadddf34d73eca32e
MaelysCC <151128589+MaelysCC@users.noreply.github.com>
2024-05-20 19:19:23 +0200

    Le menu qui fonctionne de maniere passable

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include "column.h"
 #include "cdataframe.h"
 
+
 int main()
 {
     COLUMN *mycol = create_column("My column"), *colo = create_column("afubglobg");
@@ -20,35 +21,139 @@ int main()
         //print_cdf(cdf);
         insert_cdf_col(cdf, colo);
 
-        //affichage du CDataframe
-        printf("Pour afficher tout le CDataframe, tapez 1\n");
-        printf("Pour afficher une partie des lignes tapez 2\n");
-        printf("Pour afficher une partie des colonnes tapez 3\n");
-        int affichage = 0;
 
-        scanf("%d", &affichage);
 
-        if(affichage == 1){
-            print_cdf(cdf);
-        }
-        if(affichage ==2){
-            printf("Entrez la limite de ligne minimum et maximum à afficher tel quel (ligne_min/ligne_max):\n");
-            int lig1, lig2 = 0;
-            scanf("%d/%d", &lig1, &lig2);
-            print_cdf_lig(cdf, lig1, lig2);
-        }
-        if(affichage == 3){
-            printf("Entrez la limite de colonnes minimum et maximum à afficher tel quel (ligne_min/ligne_max):\n");
-            int col1, col2 = 0;
-            scanf("%d/%d", &col1, &col2);
-            print_cdf_col(cdf, col1, col2);
-        }
+
+        int acces = 0;
+        do {//option du menu
+            printf("Pour acceder a l'affichage taper 1\n");
+            printf("Pour acceder aux operation usuelles tapez 2\n");
+            printf("Pour acceder aux analyse et statistique tapez 3\n");
+            printf("Pour revenir au menu taper 1\n");
+            printf("choisir un acces :");
+            scanf("%d", &acces);
+
+            if (acces == 1) {
+
+                printf("Pour afficher tout le CDataframe, tapez 1\n");
+                printf("Pour afficher une partie des lignes tapez 2\n");
+                printf("Pour afficher une partie des colonnes tapez 3\n");
+                printf("Pour quitter l'affichage tapez 4\n");
+                int affichage = 0;
+
+                do {
+                    printf("selectionnner votre choix:");
+                    scanf("%d", &affichage);
+
+                    if (affichage == 1) {
+                        print_cdf(cdf);
+                    }
+                    if (affichage == 2) {
+                        printf("Entrez la limite de ligne minimum et maximum à afficher tel quel (ligne_min/ligne_max):\n");
+                        int lig1, lig2 = 0;
+                        scanf("%d/%d", &lig1, &lig2);
+                        print_cdf_lig(cdf, lig1, lig2);
+                    }
+                    if (affichage == 3) {
+                        printf("Entrez la limite de colonnes minimum et maximum à afficher tel quel (ligne_min/ligne_max):\n");
+                        int col1, col2 = 0;
+                        scanf("%d/%d", &col1, &col2);
+                        print_cdf_col(cdf, col1, col2);
+                    }
+
+
+                } while (affichage != 4);
+            }
+
+            if (acces == 2){
+                int operation =0;
+                do{
+                    printf("pour ajouter une ligne tapez 1\n");
+                    printf("pour supprimer une ligne tapez 2\n");
+                    printf("pour ajouter une colonne tapez 3\n");
+                    printf("pour supprimer une colonne tapez 4\n");
+                    printf("pour renommer une colonne tapez 5\n");
+                    printf("pour rechercher une valeur tapez 6\n");
+                    printf("pour remplacer une valeur, tapez 7\n");
+                    printf("pour afficher le nom des colonnes, tapez 8\n");
+                    printf("pour quitter, tapez 9\n");
+
+                    printf("choisir l'operation :");
+                    scanf("%d", &operation);
+
+                    if(operation == 1){
+                        add_cdf_lig(cdf, 1);
+                    }
+                    if(operation == 2){
+                        del_cdf_lig(cdf, 1);
+                    }
+                    if(operation ==3){
+                        add_cdf_col(cdf, "newCol");
+                    }
+                    if (operation == 4){
+                        del_cdf_col(cdf, 3);
+                    }
+                    if(operation ==5){
+                        rename_col(cdf, 2, "titre");
+                    }
+                    if(operation == 6){
+                        if (val_existe(cdf, 52))
+                            printf("%d existe dans le CDataFrame\n", 52);
+                        else
+                            printf("%d n'existe pas dans le CDataFrame\n", 52);
+                    }
+                    if(operation == 7){
+                        cellule(cdf, 2, 3, 10);
+                    }
+                    if(operation ==8){
+                        print_noms_col(cdf);
+                    }
+
+                }while (operation != 9);
+            }
+
+
+            if (acces == 3){
+                int analyse = 0;
+
+                do{
+                    printf("pour afficher le nombre de ligne, tapez 1");
+                    printf("pour afficher le nombre de colonne, tapez 2");
+                    printf("pour afficher le nombre de valeur egal a X, tapez 3");
+                    printf("pour afficher le nombre de valeur superieur a X, tapez 4");
+                    printf("pour afficher le nombre de valeur inferieur a X, taper 5");
+
+                    printf("entrez votre choix d'affichage :");
+                    scanf("%d", &analyse);
+
+                    if(analyse == 1){
+                        printf("\nil y a %d ligne\n", nb_lig_cdf(cdf));
+                    }
+                    if(analyse == 2){
+                        printf("il y a %d colonne\n", nb_col_cdf(cdf));
+                    }
+                    if(analyse == 3){
+                        int val = 4;
+                        printf("il y a %d fois une valeur egal a 4\n", cel_val_egal(cdf, val));
+                    }
+                    if(analyse == 4){
+                        int val = 4;
+                        printf("il y a %d fois une valeur inferieur a 4\n", cel_val_inf(cdf, val));
+                    }
+                    if(analyse == 5){
+                        int val = 4;
+                        printf("il y a %d fois une valeur superieur a 4\n", cel_val_sup(cdf, val));
+                    }
+
+                }while(acces != 6);
+            }
+        }while(acces != 4);
+
 
     }
     if (choix == 2){
         return 0;
     }
-    CDF* cdf = create_cdf();
 
 
 
@@ -83,11 +188,11 @@ int main()
 
 
     delete_column(&mycol);*/
-
+/*
     insert_value(mycol, 52);
     insert_value(mycol, 59);
     insert_value(colo, 98);
-    insert_value(colo, 24);
+    insert_value(colo, 24);*/
 // donner pres enregistrer
 /*
     CDF* cdf = create_cdf();
@@ -106,7 +211,7 @@ int main()
     //rename_col(cdf, 2, "titre");
     //cellule(cdf, 2, 3, 10);
     //print_cdf(cdf);
-    if (val_existe(cdf, 52))
+    /*if (val_existe(cdf, 52))
         printf("%d existe dans le CDataFrame\n", 52);
     else
         printf("%d n'existe pas dans le CDataFrame\n", 52);
@@ -120,7 +225,7 @@ int main()
     printf("il y a %d fois une valeur egal a 4\n", cel_val_egal(cdf, val));
     printf("il y a %d fois une valeur inferieur a 4\n", cel_val_inf(cdf, val));
     printf("il y a %d fois une valeur superieur a 4\n", cel_val_sup(cdf, val));
-
+*/
 
 /*
     int lig1=0, lig2=2, col1=0, col2=2;
